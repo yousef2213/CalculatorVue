cfe6a035bbcf1547b6a24ffef236b3fac29359a6
yousef2213 <mandooyousef988@gmail.com>
2020-09-21 19:08:03 +0200

    editint

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calculator">
+    <div class="display">{{ current || "0" }}</div>
+    <div class="number" @click="clear">c</div>
+    <div class="number" @click="Sign">-/+</div>
+    <div class="number" @click="precent">%</div>
+    <div class="orange number" @click="divide">÷</div>
+    <div class="number" @click="append('7')">7</div>
+    <div class="number" @click="append('8')">8</div>
+    <div class="number" @click="append('9')">9</div>
+    <div class="orange number" @click="times">x</div>
+    <div class="number" @click="append('4')">4</div>
+    <div class="number" @click="append('5')">5</div>
+    <div class="number" @click="append('6')">6</div>
+    <div class="orange number" @click="minus">-</div>
+    <div class="number" @click="append('1')">1</div>
+    <div class="number" @click="append('2')">2</div>
+    <div class="number" @click="append('3')">3</div>
+    <div class="orange number" @click="add">+</div>
+    <div class="zero number" @click="append('0')">0</div>
+    <div class="number" @click="dot">.</div>
+    <div class="orange number" @click="equal">=</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false,
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    Sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    precent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background: #ccc;
+}
+.calculator {
+  width: 350px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  font-size: 40px;
+  margin: 60px auto;
+  overflow: hidden;
+}
+.display {
+  grid-column: 5 / 1;
+  text-align: center;
+  background: #888;
+  padding: 13px;
+}
+.zero {
+  grid-column: 3 /1;
+  padding: 10px;
+}
+.number {
+  cursor: pointer;
+  border: 1px solid #f6f6f6;
+  padding: 10px;
+}
+.orange {
+  background: #f50;
+  padding: 10px;
 }
 </style>
